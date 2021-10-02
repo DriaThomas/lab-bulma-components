ee08a8e0b6da611c0718dfbe9f7f9b899fa19376
Dria Thomas <driathomas96@gmail.com>
2021-10-02 14:58:55 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,57 @@
-import React from 'react';
+// // import React from 'react';
+// // import 'bulma/css/bulma.css';
+// import Navbar from './navbar/navbar.js';
+// import FormField from "./formfield/FormField";
+// import CoolButton from "./coolbutton/CoolButton.js";
+
+// // const App = () => {
+// //   return{
+// //     <>
+// //     <Navbar>
+// // <CoolButton isSmall isDanger className="is-rounded my-class">Button 1</CoolButton>
+// // <CoolButton isSmall isSuccess>Button 2</CoolButton>
+// //   }
+// //  </Navbar>
+// // };
+
+// // export default App;
+
+// import React from "react";
+// // import Login from "./login/Login";
+// import Signup from "./signup/Signup";
+// import "bulma/css/bulma.css";
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Navbar />
+
+//       <FormField label="Name" type="text" placeholder="e.g Dria Thomas" />
+//       <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+
+
+//       {/* <CoolButton isSmall isDanger className="is-rounded my-class">Button 1</CoolButton>
+//       <CoolButton isSmall isSuccess>Button 2</CoolButton> */}
+//       {/* <Signup /> */}
+
+
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+import React from "react";
+import Signup from "./signup/Signup";
+import "bulma/css/bulma.css";
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (
+    <div>
+      <Signup />
+    </div>
+  );
 };
 
 export default App;
